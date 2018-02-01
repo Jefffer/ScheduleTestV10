efbd7bd278905acc3dcf335de806c1de9f986bb9
Jefffer <36021969+Jefffer@users.noreply.github.com>
2018-01-31 22:23:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jeffer/scheduletestv10/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.jeffer.scheduletestv10
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun newLayout(view: View){
+        startActivity<ScheduleActivity>()
+    }
+
+}
---
+++ app/src/main/java/com/example/jeffer/scheduletestv10/ScheduleActivity.kt
@@ -0,0 +1,16 @@
+package com.example.jeffer.scheduletestv10
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Jeffer on 31/01/2018.
+ */
+class ScheduleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.schedule_view)
+    }
+
+}
