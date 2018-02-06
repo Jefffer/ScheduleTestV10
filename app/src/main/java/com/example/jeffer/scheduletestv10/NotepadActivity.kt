c49f72d3838e3907cd887dee22b65495f202772a
Jefffer <36021969+Jefffer@users.noreply.github.com>
2018-02-05 22:54:03 -0500

    Float Button

--- app/src/main/java/com/example/jeffer/scheduletestv10/NotepadActivity.kt
+++ app/src/main/java/com/example/jeffer/scheduletestv10/NotepadActivity.kt
@@ -3,6 +3,7 @@ package com.example.jeffer.scheduletestv10
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.notepad_view.*
+import org.jetbrains.anko.toast
 
 /**
  * Created by Jeffer on 4/02/2018.
@@ -15,4 +16,9 @@ class NotepadActivity : AppCompatActivity() {
         var materia = intent.getStringExtra("materia")
         notepad_title.text = materia
     }
+
+    fun save_file(){
+        toast("Nota guardada")
+    }
+
 }
